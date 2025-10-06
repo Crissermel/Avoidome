2812139fd33e3aed139e9bc298a1ac25b7ccbd71
Crissermel <126664198+Crissermel@users.noreply.github.com>
2025-10-06 12:55:57 +0200

    Delete analyses_mining directory

--- analyses_mining/components/protein_table.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def show_protein_table(df, show_stats=True):
-    st.write("## Protein Table")
-    st.dataframe(df)
-    if show_stats:
-        st.write("## Summary Stats")
-        st.write(df.describe(include='all')) 
